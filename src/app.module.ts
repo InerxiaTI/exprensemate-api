b036955a9ecfd9970690625cfabc3fb0ed3ea22c
kellytbn <kellytbn14@gmail.com>
2023-08-03 22:25:42 -0500

    Se agregan configuraciones iniciales

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,47 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import configuration from '../config/configuration';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpGeneralExceptionFilter } from './utils/exception/filter/http-general-exception.filter';
+import { HttpBusinessExceptionFilter } from './utils/exception/filter/http-business-exception.filter';
+import { HttpRequestErrorExceptionFilter } from './utils/exception/filter/request-error-exception.filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+      validationOptions: {
+        allowUnknown: false,
+        abortEarly: true,
+      },
+    }),
+    ScheduleModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: configuration().database.host,
+      port: configuration().database.port,
+      username: configuration().database.username,
+      password: configuration().database.password,
+      database: configuration().database.dbname,
+      entities: [],
+      synchronize: false,
+    }),
+    TypeOrmModule.forFeature([]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_FILTER, useClass: HttpGeneralExceptionFilter },
+    { provide: APP_FILTER, useClass: HttpBusinessExceptionFilter },
+    { provide: APP_FILTER, useClass: HttpRequestErrorExceptionFilter },
+    Logger,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { initializeTransactionalContext } from 'typeorm-transactional-cls-hooked';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import * as compression from 'compression';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  console.log(process.env.APP_ENV);
+  if (process.env.APP_ENV !== 'pdn') {
+    const options = new DocumentBuilder()
+      .setTitle('Expense Mate API')
+      .setVersion('1.0')
+      .addSecurity('basic', {
+        type: 'http',
+        scheme: 'basic',
+      })
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
+
+  // Global Middlewares
+  app.use(await compression());
+  app.use(helmet());
+  app.enableCors();
+
+  initializeTransactionalContext();
+
   await app.listen(3000);
 }
 bootstrap();
