6cf1b9e5c5053bcacedbd2718de44428da23953d
kellytbn <kellytbn14@gmail.com>
2023-08-05 12:46:08 -0500

    Servicio para listar compras de usuario con filtro

--- src/controllers/lista-compra.controller.ts
+++ src/controllers/lista-compra.controller.ts
@@ -1,9 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ListaCompraService } from '../services/lista-compra.service';
+import { StandardResponse } from '../utils/http-response/standard-response';
 
 @ApiTags('Lista de compras')
 @Controller('api/lista-compra')
 export class ListaCompraController {
   constructor(private readonly service: ListaCompraService) {}
+
+  @Get('/filter')
+  public async listarComprasConFiltro(
+    @Query() query,
+  ): Promise<StandardResponse<any[]>> {
+    return {
+      status: HttpStatus.OK,
+      body: await this.service.listarComprasConFiltro(
+        query['usuarioCreador'],
+        query['estado'],
+        query['nombre'],
+      ),
+    };
+  }
 }
--- src/services/lista-compra.service.ts
+++ src/services/lista-compra.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ListaCompra } from '../entities/lista-compra';
+import { MESSAGES_EXCEPTION } from '../utils/exception/messages-exception.enum';
+import { RequestErrorException } from '../utils/exception/request-error.exception';
 
 @Injectable()
 export class ListaCompraService {
@@ -9,4 +11,44 @@ export class ListaCompraService {
     @InjectRepository(ListaCompra)
     private listaCompraRepository: Repository<ListaCompra>,
   ) {}
+
+  public async listarComprasConFiltro(
+    usuarioCreador: number,
+    estado: string,
+    nombre: string,
+  ): Promise<any> {
+    if (!usuarioCreador) {
+      throw new RequestErrorException(MESSAGES_EXCEPTION.BUSINESS_EXCEPTION);
+    }
+
+    const sqlQuery = this.listaCompraRepository
+      .createQueryBuilder('listaCompras')
+      .select('listaCompras.id', 'id')
+      .addSelect('listaCompras.nombre', 'nombre')
+      .addSelect('listaCompras.fecha_creacion', 'fechaCreacion')
+      .addSelect('listaCompras.estado', 'estado')
+      .addSelect('listaCompras.fecha_finalizado', 'fechaFinalizado')
+      .addSelect('listaCompras.total_compras', 'totalCompras')
+      .addSelect('listaCompras.usuario_creador_fk', 'usuarioCreador')
+      .addSelect('listaCompras.codigo_generado', 'codigoGenerado')
+      .where('listaCompras.usuario_creador_fk = :usuarioCreador', {
+        usuarioCreador: usuarioCreador,
+      });
+
+    if (estado || estado === '') {
+      sqlQuery.andWhere('listaCompras.estado = :estado', {
+        estado: estado,
+      });
+    }
+
+    if (nombre || nombre === '') {
+      sqlQuery.andWhere('listaCompras.nombre like :nombre', {
+        nombre: `%${nombre}%`,
+      });
+    }
+
+    return await sqlQuery
+      .orderBy('listaCompras.fecha_creacion', 'DESC')
+      .getRawMany();
+  }
 }
